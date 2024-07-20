8f0ba0b9fb2b94c735d94ef0ff2ea6362d15dd8b
Li Fei <hefrankeleyn@gamil.com>
2024-07-20 11:44:19 +0800

    Step 1.2: 测试 BlockingQueue

--- src/test/java/io/github/hefrankeleyn/hefmq/bean/HefQueueTest.java
+++ src/test/java/io/github/hefrankeleyn/hefmq/bean/HefQueueTest.java
@@ -3,7 +3,8 @@ package io.github.hefrankeleyn.hefmq.bean;
 
 import org.junit.Test;
 
-import java.util.Queue;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.LinkedBlockingQueue;
 
 /**
  * @Date 2024/7/18
@@ -21,6 +22,27 @@ public class HefQueueTest {
 
         for (String item : hefQueue) {
             System.out.println(item);
+        }
+    }
+
+    @Test
+    public void test02() {
+        try {
+            BlockingQueue<String> blockingQueue = new LinkedBlockingQueue<>();
+            new Thread(()->{
+                try {
+                    Thread.sleep(1000 * 10L);
+                    blockingQueue.put("aa");
+                }catch (Exception e) {
+                    throw new RuntimeException(e);
+                }
+            }).start();
+            System.out.println("====> begin...");
+            blockingQueue.take();
+            String res = blockingQueue.take();
+            System.out.println("====> ok: " + res);
+        }catch (Exception e) {
+            throw new RuntimeException(e);
         }
     }
 
